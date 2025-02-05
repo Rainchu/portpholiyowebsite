9fabad500fdf54d0b9853a689d42e8e97327d32a
Rainchu <Rainchu786420@gmail.com>
2025-02-05 18:54:55 +0530

    addedresume

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -12,6 +12,10 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent {
  
+  public name : string = "Rohit Pal"
+
+  public myDetails : string = "Experienced Android developer with a designing and developing scalable APIs using .NET technologies. Currently transitioning to a full-stack .NET developer role, leveraging hands-on experience with ASP.NET Core, REST API development, and robust database management. Skilled in creating efficient, user-friendly applications and collaborating in agile environments to deliver impactful solutions. Proficient in both mobile and web application development with a commitment to continuous learning and growth."
+
 
   public icons: string[] = [
     'assets/image/facebook.png',
